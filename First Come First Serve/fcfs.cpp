fa274364fc149b3ca8e497d38948385c8bc83434
Muhammad Cahyana Bintang Fajar <bintangfajar@upi.edu>
2023-04-04 09:41:57 +0700

    a

--- First Come First Serve/fcfs.cpp
+++ First Come First Serve/fcfs.cpp
@@ -7,52 +7,63 @@
 using namespace std;
 
 // Struct untuk menyimpan informasi proses
-struct Process {
-    int id;             // ID proses
-    int arrival_time;   // Waktu kedatangan
-    int burst_time;     // Waktu burst
-    int completion_time;    // Waktu selesai
-    int waiting_time;   // Waktu waiting
+struct Process
+{
+    int id;              // ID proses
+    int arrival_time;    // Waktu kedatangan
+    int burst_time;      // Waktu burst
+    int completion_time; // Waktu selesai
+    int waiting_time;    // Waktu waiting
 };
 
-void printBar (int burst_time, int i, int id) {
+void printBar(int burst_time, int i, int id)
+{
     cout << "[";
-    for (int j = 0; j < burst_time; j++) {
-        this_thread::sleep_for(chrono::seconds(1)); 
+    for (int j = 0; j < burst_time; j++)
+    {
+        this_thread::sleep_for(chrono::seconds(1));
         cout << "-";
     }
     cout << "] Proses " << id << " (" << burst_time << "s)" << endl;
 }
 
-void printSpasi (int burst_time) {
+void printSpasi(int burst_time)
+{
     cout << " ";
-    for (int j = 0; j < burst_time; j++) {
-        // this_thread::sleep_for(chrono::seconds(1)); 
+    for (int j = 0; j < burst_time; j++)
+    {
+        // this_thread::sleep_for(chrono::seconds(1));
         cout << " ";
     }
     cout << " ";
 }
 
-int main() {
+int main()
+{
     // Inisialisasi vektor proses dengan data yang telah dihardcode
     vector<Process> processes = {
+        // {1, 0, 6, 0, 0},
+        // {2, 1, 4, 0, 0},
+        // {3, 2, 8, 0, 0},
+        // {4, 3, 1, 0, 0},
+        // {5, 2, 3, 0, 0}
+
         {1, 0, 6, 0, 0},
         {2, 1, 4, 0, 0},
         {3, 2, 8, 0, 0},
         {4, 3, 1, 0, 0},
-        {5, 2, 3, 0, 0}
-    };
+        {5, 4, 3, 0, 0}};
 
     // Urutkan proses berdasarkan waktu kedatangan
-    sort(processes.begin(), processes.end(), [](Process a, Process b) {
+    sort(processes.begin(), processes.end(), [](Process a, Process b)
+         {
         // kalo waktu arrival time nya sama
         if (a.arrival_time == b.arrival_time) {
             // diambil yang burst time nya lebih cepat
             return a.burst_time < b.burst_time;
         } else {
             return a.arrival_time < b.arrival_time;
-        }
-    });
+        } });
 
     // Inisialisasi waktu saat ini, Gantt Chart, dan total waiting time
     int current_time = 0;
@@ -63,7 +74,8 @@ int main() {
 
     // Lakukan FCFS
     cout << "Gantt Chart: " << endl;
-    for (int i = 0; i < processes.size(); i++) {
+    for (int i = 0; i < processes.size(); i++)
+    {
         // Tambahkan proses ke Gantt Chart
         gantt_chart.push_back(processes[i].id);
 
@@ -71,14 +83,17 @@ int main() {
         current_time += processes[i].burst_time;
         processes[i].completion_time = current_time;
         processes[i].waiting_time = current_time - processes[i].arrival_time - processes[i].burst_time;
-        
+
         // Tambahkan waiting time ke total_waiting_time
         total_waiting_time += processes[i].waiting_time;
 
         // Gantt Chart
-        if (i == 0) {
+        if (i == 0)
+        {
             printSpasi(spasi);
-        } else {
+        }
+        else
+        {
             spasi += processes[i - 1].burst_time + 2;
             printSpasi(spasi);
         }
@@ -86,15 +101,18 @@ int main() {
     }
 
     // Cetak informasi setiap proses
-    cout << endl << "Informasi setiap proses:" << endl;
+    cout << endl
+         << "Informasi setiap proses:" << endl;
     cout << "ID\tWaktu Kedatangan\tWaktu Burst\tWaktu Selesai\tWaktu Waiting" << endl;
-    for (int i = 0; i < processes.size(); i++) {
+    for (int i = 0; i < processes.size(); i++)
+    {
         cout << processes[i].id << "\t" << processes[i].arrival_time << "\t\t\t" << processes[i].burst_time << "\t\t" << processes[i].completion_time << "\t\t" << processes[i].waiting_time << endl;
     }
 
     // Cetak average waiting time
     float avg_waiting_time = (float)total_waiting_time / processes.size();
-    cout << endl << "Average Waiting Time: " << avg_waiting_time << endl;
+    cout << endl
+         << "Average Waiting Time: " << avg_waiting_time << endl;
 
     return 0;
 }
--- Round Robin/round_robin.cpp
+++ Round Robin/round_robin.cpp
@@ -3,28 +3,33 @@
 using namespace std;
 
 // Struct untuk menyimpan informasi proses
-struct Process {
-    int id;             // ID proses
-    int arrival_time;   // Waktu kedatangan
-    int burst_time;     // Waktu burst
-    int completion_time;    // Waktu selesai
-    int waiting_time;   // Waktu waiting
+struct Process
+{
+    int id;              // ID proses
+    int arrival_time;    // Waktu kedatangan
+    int burst_time;      // Waktu burst
+    int completion_time; // Waktu selesai
+    int waiting_time;    // Waktu waiting
     int remaining_time;
     bool isDone;
 };
 
-void printBar (int burst_time, int id) {
+void printBar(int burst_time, int id)
+{
     cout << "[";
-    for (int j = 0; j < burst_time; j++) {
-        this_thread::sleep_for(chrono::seconds(1)); 
+    for (int j = 0; j < burst_time; j++)
+    {
+        this_thread::sleep_for(chrono::seconds(1));
         cout << "-";
     }
     cout << "]" << endl;
 }
 
-void printSpasi (int burst_time) {
-    for (int j = 0; j < burst_time; j++) {
-        // this_thread::sleep_for(chrono::seconds(1)); 
+void printSpasi(int burst_time)
+{
+    for (int j = 0; j < burst_time; j++)
+    {
+        // this_thread::sleep_for(chrono::seconds(1));
         cout << " ";
     }
 }
@@ -34,7 +39,8 @@ bool isDone(Process it)
     return it.remaining_time <= 0;
 }
 
-int main() {
+int main()
+{
     int size;
     vector<Process> processes;
     int quantum_time;
@@ -61,35 +67,40 @@ int main() {
     cin >> quantum_time;
 
     // Urutkan proses berdasarkan waktu kedatangan
-    sort(processes.begin(), processes.end(), [](Process a, Process b) {
-        return a.arrival_time < b.arrival_time;
-    });
+    sort(processes.begin(), processes.end(), [](Process a, Process b)
+         { return a.arrival_time < b.arrival_time; });
 
     // Inisialisasi waktu saat ini, Gantt Chart, dan total waiting time
     int current_time = 0;
     int total_waiting_time = 0;
 
+    if (processes[0].arrival_time > current_time)
+    {
+        current_time = processes[0].arrival_time;
+    }
+
     int spasi = 0;
 
     bool flag = true;
 
-    cout << endl << "Gantt Chart: " << endl;
+    cout << endl
+         << "Gantt Chart: " << endl;
     while (flag)
     {
         flag = false;
         for (int i = 0; i < processes.size(); i++)
         {
-            if (!processes[i].isDone)
+            if (!processes[i].isDone && processes[i].arrival_time <= current_time)
             {
                 if (processes[i].remaining_time - quantum_time > 0)
                 {
                     cout << "Proses " << processes[i].id << " (" << quantum_time << "s) | ";
-            
+
                     printSpasi(spasi);
                     spasi += quantum_time + 2;
-                    
+
                     printBar(quantum_time, processes[i].id);
-                    
+
                     current_time += quantum_time;
                     processes[i].remaining_time -= quantum_time;
 
@@ -98,9 +109,9 @@ int main() {
                 else
                 {
                     cout << "Proses " << processes[i].id << " (" << processes[i].remaining_time << "s) | ";
-            
+
                     printSpasi(spasi);
-                    spasi += processes[i].remaining_time + 2;        
+                    spasi += processes[i].remaining_time + 2;
 
                     printBar(processes[i].remaining_time, processes[i].id);
 
@@ -119,15 +130,18 @@ int main() {
     }
 
     // Cetak informasi setiap proses
-    cout << endl << "Informasi setiap proses:" << endl;
+    cout << endl
+         << "Informasi setiap proses:" << endl;
     cout << "ID\tWaktu Kedatangan\tWaktu Burst\tWaktu Selesai\tWaktu Waiting" << endl;
-    for (int i = 0; i < processes.size(); i++) {
+    for (int i = 0; i < processes.size(); i++)
+    {
         cout << processes[i].id << "\t" << processes[i].arrival_time << "\t\t\t" << processes[i].burst_time << "\t\t" << processes[i].completion_time << "\t\t" << processes[i].waiting_time << endl;
     }
 
     // Cetak average waiting time
     float avg_waiting_time = (float)total_waiting_time / processes.size();
-    cout << endl << "Average Waiting Time: " << avg_waiting_time << endl;
+    cout << endl
+         << "Average Waiting Time: " << avg_waiting_time << endl;
 
     return 0;
 }
